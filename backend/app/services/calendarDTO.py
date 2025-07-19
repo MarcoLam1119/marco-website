193346302ce497d11bf2aa9572331bd72d23d540
MarcoLam1119 <77959065+MarcoLam1119@users.noreply.github.com>
2025-07-19 14:11:26 +0800

    Create calendarDTO.py

---
+++ backend/app/services/calendarDTO.py
@@ -0,0 +1,22 @@
+# TODO calendar dto 
+# add new event 
+# update event 
+# delete event
+# list event 
+# select event
+
+def get_event(eventId):
+    return eventId;
+
+def list_event():
+    return "all event list"
+
+def add_event(event):
+    return event
+
+def update_event(event):
+    return event
+
+def delete_event(eventId):
+    return ""
+    
