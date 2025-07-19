644eecb6977ba4dc97cc079bd7ff8752312e6ee7
MarcoLam1119 <77959065+MarcoLam1119@users.noreply.github.com>
2025-07-19 14:20:54 +0800

    Create calndarEventModels.py

---
+++ backend/app/models/calndarEventModels.py
@@ -0,0 +1,14 @@
+# model of event
+
+{
+	eventId : int
+	eventName : string
+	eventDescription : string 
+	startDate : datetime
+	EndDate : datetime
+	isFullDate : bool
+    starTime : datetime
+    endTime : datetime
+    isPublish : bool
+    isDelete : bool
+}
